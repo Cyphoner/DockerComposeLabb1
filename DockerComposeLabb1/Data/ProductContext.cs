49682e9a040607975aa43e33c03800cf15f98b3b
Vincent Kaveh <vincent.kaveh@iths.se>
2024-12-13 14:02:30 +0100

    Add project files.

---
+++ DockerComposeLabb1/Data/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using DockerComposeLabb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerComposeLabb1.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) :base(options) { }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
